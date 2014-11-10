d21f0d2a3c8e0114a41a3bc7fb1fa8b765a66174
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2014-11-10 09:28:12 +0100

    be json my friend (told me a friend of mine)

--- app/controllers/tuits_controller.rb
+++ app/controllers/tuits_controller.rb
@@ -1,9 +1,14 @@
 class TuitsController < ApplicationController
-  TUITS_FILE = 'tuits'
   protect_from_forgery :except => :create
 
   def index
     @tuits = Tuit.all
+    respond_to do |format|
+      format.html
+      format.json { 
+        render :json => @tuits.map{|t| {t.piido => t.updated_at.to_i}}
+      }
+    end
   end
 
   def create
